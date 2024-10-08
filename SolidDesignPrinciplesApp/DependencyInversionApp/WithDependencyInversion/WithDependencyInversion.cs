e13de8604b2d0349ecdb16d248af6ab124f6c62d
deepakranapro@outlook.com <deepakranapro@outlook.com>
2024-10-08 14:07:57 +0530

    Implemented DependencyInversion

---
+++ SolidDesignPrinciplesApp/DependencyInversionApp/WithDependencyInversion/WithDependencyInversion.cs
@@ -0,0 +1,51 @@
+﻿namespace DependencyInversionApp.WithDependencyInversion
+{
+    public interface IDevice
+    {
+        void TurnOn();
+        void TurnOff();
+    }
+
+    public class Light : IDevice
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Light is on.");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Light is off.");
+        }
+    }
+
+    public class Switch
+    {
+        private IDevice _device;
+
+        public Switch(IDevice device)
+        {
+            _device = device;
+        }
+
+        public void Operate(bool on)
+        {
+            if (on)
+                _device.TurnOn();
+            else
+                _device.TurnOff();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IDevice light = new Light();
+            Switch lightSwitch = new Switch(light);
+
+            lightSwitch.Operate(true);  // Output: Light is on.
+            lightSwitch.Operate(false); // Output: Light is off.
+        }
+    } 
+}
---
+++ SolidDesignPrinciplesApp/DependencyInversionApp/WithoutDependencyInversion/WithoutDependencyInversion.cs
@@ -0,0 +1,33 @@
+﻿namespace DependencyInversionApp.WithoutDependencyInversion
+{
+    public class Light
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Light is on.");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Light is off.");
+        }
+    }
+
+    public class Switch
+    {
+        private Light _light;
+
+        public Switch(Light light)
+        {
+            _light = light;
+        }
+
+        public void Operate(bool on)
+        {
+            if (on)
+                _light.TurnOn();
+            else
+                _light.TurnOff();
+        }
+    }
+}
